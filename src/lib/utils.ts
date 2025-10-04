0c7d798a4fc49709c6b7948244e4c1a21edbee45
Ryan Weiss <rw3iss@gmail.com>
2025-10-03 23:46:04 -0400

    latest

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -172,10 +172,10 @@ function getCallStack(): string {
 }
 
 export const executePromisesSequentially = async (promises: (() => Promise<any>)[]): Promise<any[]> => {
-  const results: any[] = [];
-  for (const promiseFactory of promises) {
-    const result = await promiseFactory(); // Await each promise before proceeding
-    results.push(result);
-  }
-  return results;
+	const results: any[] = [];
+	for (const promiseFactory of promises) {
+		const result = await promiseFactory(); // Await each promise before proceeding
+		results.push(result);
+	}
+	return results;
 }
