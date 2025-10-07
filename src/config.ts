bc2e6516096477b6fc817ec18c9877688a3be208
Ryan Weiss <rw3iss@gmail.com>
2025-10-06 23:37:29 -0400

    set LOG_WARNINGS_ALWAYS default to false

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,6 @@ const tryParseBool = (b: any, def: boolean | undefined = undefined) => {
 
 export const LOG_ERROR_TRACES = process.env.LOG_ERROR_TRACES === 'true';
 export const ALWAYS_LOG_ERRORS = tryParseBool(process.env.LOG_ERRORS_ALWAYS, true);
-export const ALWAYS_LOG_WARNINGS = tryParseBool(process.env.LOG_WARNINGS_ALWAYS, true);
+export const ALWAYS_LOG_WARNINGS = tryParseBool(process.env.LOG_WARNINGS_ALWAYS, false);
 export const COLORS_ENABLED = tryParseBool(process.env.LOG_COLORS_ENABLED, true);
 export const DEFAULT_LOG_COLOR = process.env.LOG_COLOR_DEFAULT;
